7f95b935a61daa5ad2ffa6ba5078bfbe6a9126e0
jonataswalker <jonataswalker@gmail.com>
2016-04-20 22:49:37 -0300

    Update example

--- examples/example.js
+++ examples/example.js
@@ -3,29 +3,30 @@ hljs.configure({
 })
 hljs.initHighlightingOnLoad();
 
-var time2 = document.getElementById('time2');
-
 var timepicker = new TimePicker(['time', 'link'], {
-  lang: 'en'
+  lang: 'en',
+  theme: 'blue-grey'
 });
-timepicker.on('open', function(evt){
-  
+
+timepicker.on('open', function(evt) {
+/*  
   console.info('opened');
-  console.info(evt);
+  console.info(evt);*/
   
 });
-timepicker.on('close', function(evt){
-  
+timepicker.on('close', function(evt) {
+/*  
   console.info('closed');
-  console.info(evt);
+  console.info(evt);*/
   
 });
-timepicker.on('change', function(evt){
+var time2 = document.getElementById('time2');
+timepicker.on('change', function(evt) {
   
   console.info('change');
   console.info(evt);
   
-  var value = '' + (evt.hour || '00') + ':' + (evt.minute || '00');
+  var value = (evt.hour || '00') + ':' + (evt.minute || '00');
   
   if (evt.element.id == 'link') {
     time2.value = value;
